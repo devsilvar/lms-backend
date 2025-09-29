e09abef877b38f6ec24bdadf68c1b6c5fef9d7e3
refsilva <refsilva@yahoo.com>
2025-09-29 12:00:36 +0100

    new fiex

--- dist/src/app.js
+++ dist/src/app.js
@@ -4,9 +4,10 @@ import userRoutes from "./routes/userRoutes.js";
 import cors from "cors";
 const app = express();
 app.use(cors({
-    origin: process.env.FRONTEND_URL,
-    // ✅ your frontend's URL
-    credentials: true, // ✅ allow cookies/authorization headers
+    origin: function (origin, callback) {
+        callback(null, origin); // allow the requesting origin dynamically
+    },
+    credentials: true, // allow cookies/authorization headers
 }));
 app.use(express.json());
 app.use("/api/user", userRoutes);
--- src/app.ts
+++ src/app.ts
@@ -6,13 +6,12 @@ import { userInfo } from "os";
 
 
 const app = express();
-app.use(
-  cors({
-    origin: process.env.FRONTEND_URL,
-    // ✅ your frontend's URL
-    credentials: true, // ✅ allow cookies/authorization headers
-  })
-);
+app.use(cors({
+  origin: function(origin, callback) {
+    callback(null, origin); // allow the requesting origin dynamically
+  },
+  credentials: true, // allow cookies/authorization headers
+}));
 app.use(express.json());
 app.use("/api/user" , userRoutes);
 app.use("/api/auth", authRoutes);
